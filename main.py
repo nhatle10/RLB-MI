642401bc9d109e103b5f147bf52835c882221905
mbyhphat <22521067@gm.uit.edu.vn>
2025-05-28 11:35:10 +0700

    feat: remove random and add start, end index

--- main.py
+++ main.py
@@ -7,12 +7,12 @@ import torch.nn.functional as F
 import argparse
 from generator import Generator
 from classify import *
-from utils import * 
+from utils import *
 from SAC import Agent
 from attack import inversion
 
 parser = argparse.ArgumentParser(description="RLB-MI")
-parser.add_argument('-model_name', default='VGG16')
+parser.add_argument("-model_name", default="VGG16")
 parser.add_argument("-max_episodes", type=int, default=40000)
 parser.add_argument("-max_step", type=int, default=1)
 parser.add_argument("-seed", type=int, default=42)
@@ -20,6 +20,10 @@ parser.add_argument("-alpha", type=float, default=0)
 parser.add_argument("-n_classes", type=int, default=1000)
 parser.add_argument("-z_dim", type=int, default=100)
 parser.add_argument("-n_target", type=int, default=100)
+parser.add_argument(
+    "-start", type=int, default=0, help="Start index for target classes"
+)
+parser.add_argument("-end", type=int, default=100, help="End index for target classes")
 args = parser.parse_args()
 
 if __name__ == "__main__":
@@ -36,30 +40,30 @@ if __name__ == "__main__":
     G = Generator(z_dim)
     G = nn.DataParallel(G).cuda()
     G = G.cuda()
-    ckp_G = torch.load('/kaggle/input/weightcs106/weights/CelebA.tar')['state_dict']
+    ckp_G = torch.load("/kaggle/input/weightcs106/weights/CelebA.tar")["state_dict"]
     load_my_state_dict(G, ckp_G)
     G.eval()
 
     if model_name == "VGG16":
         T = VGG16(n_classes)
-        path_T = '/kaggle/input/weightcs106/weights/VGG16.tar'
-    elif model_name == 'ResNet-152':
+        path_T = "/kaggle/input/weightcs106/weights/VGG16.tar"
+    elif model_name == "ResNet-152":
         T = IR152(n_classes)
-        path_T = '/kaggle/input/weightcs106/weights/ResNet-152.tar'
+        path_T = "/kaggle/input/weightcs106/weights/ResNet-152.tar"
     elif model_name == "Face.evoLVe":
         T = FaceNet64(n_classes)
-        path_T = '/kaggle/input/weightcs106/weights/Face.evoLVe.tar'
+        path_T = "/kaggle/input/weightcs106/weights/Face.evoLVe.tar"
 
     T = torch.nn.DataParallel(T).cuda()
     ckp_T = torch.load(path_T)
-    T.load_state_dict(ckp_T['state_dict'], strict=False)
+    T.load_state_dict(ckp_T["state_dict"], strict=False)
     T.eval()
 
     E = FaceNet(n_classes)
-    path_E = '/kaggle/input/weightcs106/weights/Eval.tar'
+    path_E = "/kaggle/input/weightcs106/weights/Eval.tar"
     E = torch.nn.DataParallel(E).cuda()
     ckp_E = torch.load(path_E)
-    E.load_state_dict(ckp_E['state_dict'], strict=False)
+    E.load_state_dict(ckp_E["state_dict"], strict=False)
     E.eval()
 
     def seed_everything(seed: int = 42):
@@ -78,12 +82,29 @@ if __name__ == "__main__":
     cnt5 = 0
 
     identities = range(n_classes)
-    targets = random.sample(identities, n_target)
+    targets = list(identities)[:n_target]
+    targets = targets[args.start : args.end]
 
     for i in targets:
-        agent = Agent(state_size=z_dim, action_size=z_dim, random_seed=seed, hidden_size=256, action_prior="uniform")
-        recon_image = inversion(agent, G, T, alpha, z_dim=z_dim, max_episodes=max_episodes, max_step=max_step, label=i, model_name=model_name)
-        _, output= E(low2high(recon_image))
+        agent = Agent(
+            state_size=z_dim,
+            action_size=z_dim,
+            random_seed=seed,
+            hidden_size=256,
+            action_prior="uniform",
+        )
+        recon_image = inversion(
+            agent,
+            G,
+            T,
+            alpha,
+            z_dim=z_dim,
+            max_episodes=max_episodes,
+            max_step=max_step,
+            label=i,
+            model_name=model_name,
+        )
+        _, output = E(low2high(recon_image))
         eval_prob = F.softmax(output[0], dim=-1)
         top_idx = torch.argmax(eval_prob)
         _, top5_idx = torch.topk(eval_prob, 5)
@@ -96,5 +117,8 @@ if __name__ == "__main__":
 
         acc = cnt / total
         acc5 = cnt5 / total
-        print("Classes {}/{}, Accuracy : {:.3f}, Top-5 Accuracy : {:.3f}".format(total, n_target, acc, acc5))
-    
+        print(
+            "Classes {}/{}, Accuracy : {:.3f}, Top-5 Accuracy : {:.3f}".format(
+                total, n_target, acc, acc5
+            )
+        )
