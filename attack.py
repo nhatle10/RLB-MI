b2c3a94089599244c876d81d5d8800fc070f6791
mbyhphat <hhp031104ft@gmail.com>
2025-06-06 10:20:30 +0700

    fix: type error

--- attack.py
+++ attack.py
@@ -165,7 +165,7 @@ def inversion(
 
             print(f"Episode {i_episode}:")
             print(
-                f"  Model E - Accuracy: {E_top1_correct.float().mean():.4f}, Top-5 accuracy: {E_top5_correct.float().mean():.4f}"
+                f"  Top-1 Accuracy: {E_top1_correct.float().mean():.4f}, Top-5 accuracy: {E_top5_correct.float().mean():.4f}"
             )
 
         if mean_score >= best_score:
